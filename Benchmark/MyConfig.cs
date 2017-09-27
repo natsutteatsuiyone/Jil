641d223cdbc0a1bd5049b8237c8f9e26f8b774e7
Tornhoof <Tornhoof@gmail.com>
2017-09-27 19:29:34 +0200

    Benchmark

--- Benchmark/MyConfig.cs
+++ Benchmark/MyConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Environments;
 using BenchmarkDotNet.Horology;
 using BenchmarkDotNet.Jobs;
@@ -14,14 +15,8 @@ namespace Benchmark
     {
         public MyConfig()
         {
-            Add(
-                Job.Dry
-                    .With(Platform.X64)
-                    .With(Jit.RyuJit)
-                    .With(Runtime.Clr)
-                    .WithLaunchCount(5)
-                    .WithIterationTime(TimeInterval.Millisecond * 200)
-                    .WithId("MySuperJob")); // IMPORTANT: Id assignment should be the last call in the chain or the id will be lost.
+            Add(Job.Default);
+            Add(new MemoryDiagnoser());
         }
     }
 }
